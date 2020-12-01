dc8ed7f4d50f0138a5fd89b7433aa198ef970e7c
Louis <l.lefebvre@rubika-edu.com>
2020-12-01 16:13:38 +0100

    setup

---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Cannonballs/Cannonballs_Scripts/Gabarit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gabarit : MonoBehaviour
+{
+    [SerializeField]
+    private float baseSpeed;
+    [SerializeField]
+    private float speedModifier;
+    public GameObject Player;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = (Player.transform.position - transform.position).normalized * (baseSpeed * speedModifier);
+    }
+}
---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Cannonballs/Cannonballs_Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float speedModifier;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(inputHorizontal, inputVertical, 0f);
+        rb.velocity = new Vector2(movement.x, movement.y).normalized * (speed * speedModifier);
+    }
+}
